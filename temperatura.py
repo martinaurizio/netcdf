390a55173e7333ce81ca11ba5a628cd8a6398488
martinaurizio <martinaurizio@gmail.com>
2016-06-30 12:16:16 +0200

    Sostituito il grafico precedente con barre di colore relative alle temperature nei vari livelli

--- temperatura.py
+++ temperatura.py
@@ -2,6 +2,7 @@
 from sys import argv, exit
 import numpy as np
 from netCDF4 import Dataset
+import matplotlib.pyplot as mplt
 
 #errore in caso di mancato inserimento del percorso del file da cui prelevare i dati
 if len(argv)<=1:
@@ -12,6 +13,12 @@ fg = Dataset(percorso, "r")
 atmc = fg.groups["atmospheric_components"]
 t = atmc.variables['T']
 temp = np.array(t)
-medie = np.mean(temp, axis=0)
-print(medie)
+n_al = temp.shape[1]
 fg.close()
+medie = np.mean(temp, axis=0)
+medie -= 272.15
+print(medie)
+medie_mtx = medie.reshape(61, 1)
+mplt.pcolor(medie_mtx)
+mplt.ylim(0,61)
+mplt.show()
